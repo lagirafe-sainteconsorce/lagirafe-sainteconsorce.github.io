981336ca0280175f34f53e71b60ce2e6346d9879
Frédéric Schaffar <f@fredericschaffar.com>
2023-11-21 18:12:31 +0100

    je suis revenu au each initial, j'ai tenté dans event.js de passer en var la date now. Mais je ne vois pas comment la charger ensuite. Il faut peut-être essayer de faire une comparaison à ce moment là avec la date de l'evenement et du coup avoir 2 each imbriqués

---
+++ site/src/helpers/event.js
@@ -0,0 +1,7 @@
+// Example file src/helpers/event.js
+module.exports = function(options) {
+    // tentative de passer une var avec la date now
+    const start = Date.now();
+    return start;
+    console.log(start);
+}
